f5fdce603ebab82a05a38d71a204124c4562b542
drip <tvazquez92@gmail.com>
2024-10-18 19:12:45 -0400

    Initial commit

--- Parks/ContentView.swift
+++ Parks/ContentView.swift
@@ -1,21 +1,79 @@
-//
-//  ContentView.swift
-//  Parks
-//
-//  Created by Tony Vazquez on 10/18/24.
-//
+import SwiftUI
 
 import SwiftUI
 
 struct ContentView: View {
+    @State private var parks: [Park] = []
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack { // Wrap the top-level view in a NavigationStack
+            ScrollView {
+                LazyVStack {
+                    ForEach(parks) { park in
+                        NavigationLink(value: park) { // Add NavigationLink for each park
+                            ParkRow(park: park)
+                        }
+                    }
+                }
+            }
+            .onAppear(perform: {
+                Task {
+                    await fetchParks()
+                }
+            })
+            .navigationDestination(for: Park.self) { park in // Define the navigation destination
+                ParkDetailView(park: park) // ParkDetailView will be shown on navigation
+            }
+            .navigationTitle("National Parks")
         }
-        .padding()
+    }
+
+    private func fetchParks() async {
+        // URL for the API endpoint (replace with your API key)
+        let url = URL(string: "https://developer.nps.gov/api/v1/parks?stateCode=ca&api_key=z1jy9jjf0iq8JyIdSTNTl4B74fhPo7c0uDAD9XfT")!
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url)
+            let parksResponse = try JSONDecoder().decode(ParksResponse.self, from: data)
+            self.parks = parksResponse.data
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
+}
+struct ParkRow: View {
+    let park: Park
+
+    var body: some View {
+        
+        // Park row
+        Rectangle()
+            .aspectRatio(4/3, contentMode: .fit)
+            .overlay {
+            
+                let image = park.images.first
+                let urlString = image?.url
+                let url = urlString.flatMap { string in
+                    URL(string: string)
+                }
+                
+            
+                AsyncImage(url: url) { image in
+                    image
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                } placeholder: {
+                    Color(.systemGray4)
+                }
+            }
+            .overlay(alignment: .bottomLeading) {
+                Text(park.name)
+                    .font(.title)
+                    .bold()
+                    .foregroundStyle(.white)
+                    .padding()
+            }
+            .cornerRadius(16)
+            .padding(.horizontal)
     }
 }
 
---
+++ Parks/ParksResponse.swift
@@ -0,0 +1,44 @@
+//
+//  ParksResponse.swift
+//  Parks
+//
+//  Created by Tony Vazquez on 10/18/24.
+//
+
+import Foundation
+
+struct ParksResponse: Codable {
+    let data: [Park]
+}
+
+struct Park: Codable, Identifiable, Hashable, Equatable {
+    let id: String
+    let fullName: String
+    let description: String
+    let latitude: String
+    let longitude: String
+    let images: [ParkImage]
+    let name: String
+    
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(id)
+    }
+}
+
+struct ParkImage: Codable, Identifiable, Equatable {
+    let title: String
+    let caption: String
+    let url: String
+    
+    var id: String {
+        url
+    }
+}
+extension Park {
+    static var mocked: Park {
+        let jsonUrl = Bundle.main.url(forResource: "park_mock", withExtension: "json")!
+        let data = try! Data(contentsOf: jsonUrl)
+        let park = try! JSONDecoder().decode(Park.self, from: data)
+        return park
+    }
+}
