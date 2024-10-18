ecd4f5f286ec970558e26495e4594e0fa962dbcf
drip <tvazquez92@gmail.com>
2024-10-18 13:17:59 -0400

    Initial Commit

---
+++ Parks/ParksApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParksApp.swift
+//  Parks
+//
+//  Created by Tony Vazquez on 10/18/24.
+//
+
+import SwiftUI
+
+@main
+struct ParksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
